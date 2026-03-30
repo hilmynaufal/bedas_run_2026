fb15a6da8974c05e85f9fb4a6b73e24f25729a47
Hilmy Naufal <hilmyblaze@gmail.com>
2026-03-31 05:55:27 +0700

    refactor: update transaction reference ID to use dynamic variable instead of hardcoded value

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.post('/payment', async (req, res) => {
   const PRICE = ['1000'];
   const AMOUNT = '1000';
 
-  const txReferenceId = 'ID1234' || `ID${Date.now()}`;
+  const txReferenceId = referenceId || `ID${Date.now()}`;
 
   const fd = formData || {};
 
@@ -96,8 +96,8 @@ app.post('/payment', async (req, res) => {
     buyerPhone: buyerPhone || fd['Nomor Whatsapp Aktif'] || null,
     buyerEmail: buyerEmail || fd['Email'] || null,
     //test
-    // referenceId: txReferenceId,
-    referenceId: 'ID1234'
+    referenceId: txReferenceId,
+    // referenceId: 'ID1234'
   };
 
   console.log('[payment] notifyUrl:', body.notifyUrl);
